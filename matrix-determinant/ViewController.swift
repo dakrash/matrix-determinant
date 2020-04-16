53b8bc5d4e7b122325ea7d5f021c6a788a5c3444
Darya <contact@dkrash.ru>
2020-04-16 18:08:15 +0400

    Initial Commit

---
+++ matrix-determinant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  matrix-determinant
+//
+//  Created by darya on 16.04.2020.
+//  Copyright © 2020 darya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
